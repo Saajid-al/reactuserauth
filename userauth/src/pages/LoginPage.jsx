401e928c9aa9292fa26a39093d5cf86561146cdc
Saaji <saajid.aliyar@outlook.com>
2024-10-21 00:58:31 -0400

    new ui pt2

--- userauth/src/pages/LoginPage.jsx
+++ userauth/src/pages/LoginPage.jsx
@@ -4,8 +4,8 @@ import mortgagedashlogo from '../assets/mortgagedashlogo.png';
 export default function MortgageDash() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white relative">
-      <div className="absolute left-0 top-0 h-full w-1/4 bg-gray-100 rounded-br-full"></div>
-      <div className="absolute right-0 top-0 h-full w-1/4 bg-gray-100 rounded-bl-full"></div>
+      <div className="absolute left-0 top-0 h-full w-1/4 bg-gray-900 rounded-br-full"></div>
+      <div className="absolute right-0 top-0 h-full w-1/4 bg-gray-900 rounded-bl-full"></div>
 
       <div className="relative z-10 flex flex-col items-center text-center p-10">
         <img 
@@ -18,7 +18,7 @@ export default function MortgageDash() {
           Pre-Qualify Clients <span className="bg-gradient-to-r from-green-600 to-green-950 text-transparent bg-clip-text">with ease</span>
         </p>
         <button className="btn btn-accent py-4 px-8 text-lg font-semibold rounded-full shadow-lg transition-transform duration-300 hover:scale-105 flex items-center justify-center h-12">
-          <span className="flex text-white items-center justify-center h-full">Get Started</span>
+          <span className="flex text-white items-center justify-center h-full b-10">Get Started</span>
         </button>
       </div>
     </div>
